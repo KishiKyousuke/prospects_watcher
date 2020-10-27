890212d99d76b67eaf13cb2ecb8f6c0415625987
KishiKyousuke <carp011808pg@icloud.com>
2020-10-27 18:48:01 +0900

    バッター一覧ページとピッチャー一覧ページを実装

--- Gemfile
+++ Gemfile
@@ -54,3 +54,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'nokogiri'
 gem 'devise'
+gem "slim-rails"
+gem "html2slim"
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :pitchers, only: [:index]
   resources :batters, only: [:index]
   devise_for :users
-  root 'players#index'
+  root 'batters#index'
 end
