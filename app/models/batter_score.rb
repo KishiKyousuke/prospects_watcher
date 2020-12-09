faf65862a15a9070190a2594ba45d05d1dda400d
KishiKyosuke <carp011808pg@icloud.com>
2020-12-09 16:19:57 +0900

    BatterScoreクラスとPitcherScoreクラスを作成
    - Rakeタスクから野手と投手のDB保存の処理を切り出すため

---
+++ app/models/batter_score.rb
@@ -0,0 +1,38 @@
+class BatterScore
+  def initialize(scores, team)
+    @number = scores[0]
+    @url = scores[1]
+    @name = scores[2]
+    @team = team
+    @batting_average = scores[3]
+    @home_run = scores[10]
+    @runs_batted_in = scores[12]
+    @stolen_base = scores[19]
+    @on_base_percentage = scores[22]
+    @on_base_plus_slugging = scores[24]
+    @walks = scores[15]
+    @hit_by_pitch = scores[16]
+    @scoring_position_batting_average = scores[25]
+    @strikeout = scores[14]
+    @error = scores[26]
+  end
+
+  def reflect_in_db
+    batter = Batter.find_or_initialize_by(url: @url)
+    batter.update(number: @number,
+                  url: @url,
+                  name: @name,
+                  team: @team,
+                  batting_average: @batting_average,
+                  home_run: @home_run,
+                  runs_batted_in: @runs_batted_in,
+                  stolen_base: @stolen_base,
+                  on_base_percentage: @on_base_percentage,
+                  on_base_plus_slugging: @on_base_plus_slugging,
+                  walks: @walks,
+                  hit_by_pitch: @hit_by_pitch,
+                  scoring_position_batting_average: @scoring_position_batting_average,
+                  strikeout: @strikeout,
+                  error: @error)
+  end
+end
