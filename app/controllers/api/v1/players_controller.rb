2ce93f49f732f631d87fe525ec434a96cff61085
KishiKyosuke <carp011808pg@icloud.com>
2020-12-10 14:26:38 +0900

    新たに`players_controller.rb`と`api/v1/players_controller.rb`を作成
    - all_teams.vueに渡したいデータは全選手のデータ。データを渡すコントローラを1つにまとめて、その中でBatter.allとPitcher.allを行い、データの加工をすれば良い

---
+++ app/controllers/api/v1/players_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::PlayersController < ActionController::API
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,12 @@ Rails.application.routes.draw do
     get :favorite_batters, on: :collection
     get :favorite_pitchers, on: :collection
   end
+  resources :players, only: :index
+  namespace :api, { format: 'json' } do
+    namespace :v1 do
+      resources :players, only: [:index]
+    end
+  end
   resources :registered_players, only: :index
   root 'registered_players#index'
 end
