182b27788a3b112aa729eb171d51ca81a15b1d96
KishiKyosuke <carp011808pg@icloud.com>
2020-12-11 16:45:18 +0900

    チーム別選手一覧のコンポーネントを作成
    - 現段階では育成選手もDBに登録されているため`001`などが前に来てしまっている
    - 選手名の横に登録用のボタンを追加予定
    - できればスクロールしても開いているチームのチーム名が画面上部に固定されるようにしたい

---
+++ app/javascript/team_players.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app" class="container">
+    <div class="batters-box">
+      <h3>野手</h3>
+      <el-table
+          :data="players[selectedTeam]['batters']"
+          style="width: 100%">
+        <el-table-column
+            prop="number"
+            label="背番号"
+            width="90">
+        </el-table-column>
+        <el-table-column
+            prop="name"
+            label="名前"
+            width="180">
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class="pitchers-box">
+      <h3>投手</h3>
+      <el-table
+          :data="players[selectedTeam]['pitchers']"
+          style="width: 100%">
+        <el-table-column
+            prop="number"
+            label="背番号"
+            width="90">
+        </el-table-column>
+        <el-table-column
+            prop="name"
+            label="名前"
+            width="180">
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  data: function () {
+    return {
+      players: null
+    }
+  },
+  props: ["selectedTeam"]
+  ,
+  mounted() {
+    axios.get('/api/v1/players').then(response => this.players = response.data)
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+}
+
+.batters-box, .pitchers-box {
+  width: 350px;
+}
+</style>
