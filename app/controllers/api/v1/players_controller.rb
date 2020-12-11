93544d67bf6684dc21a668b229072d625d1f7f1b
KishiKyosuke <carp011808pg@icloud.com>
2020-12-11 12:06:24 +0900

    api/v1/players_controller.rb`のindexアクションを、JSONのデータを返すように変更

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::PlayersController < ActionController::API
   def index
+    batters = Batter.select(:id, :number, :name, :team)
+    pitchers = Pitcher.select(:id, :number, :name, :team)
+    players_data_formatter = PlayersDataFormatter.new(batters, pitchers)
+    players_data_formatter.run
+    players_data = players_data_formatter.all_data
+    render :json => players_data
   end
 end
