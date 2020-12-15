6a04401160b47941c63b8cd677c7c829ebdc7991
KishiKyosuke <carp011808pg@icloud.com>
2020-12-15 14:07:18 +0900

    `registered_players_controller.rb`を作成
    - apiからユーザーの登録選手情報を取得するためのコントローラ

---
+++ app/controllers/api/v1/registered_players_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::RegisteredPlayersController < ActionController::API
+  before_action :authenticate_user!
+
+  def index
+    favorite_players = {batters: current_user.favorite_batters.all, pitchers: current_user.favorite_pitchers.all}
+    render json: favorite_players
+  end
+end
