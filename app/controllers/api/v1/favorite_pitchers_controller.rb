4c7575c618b9d86a1dc72a1b49b02becb968fd58
KishiKyosuke <carp011808pg@icloud.com>
2020-12-15 17:09:38 +0900

    rubocopによる修正の反映

--- app/controllers/api/v1/favorite_pitchers_controller.rb
+++ app/controllers/api/v1/favorite_pitchers_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Api::V1::FavoritePitchersController < ActionController::API
   before_action :authenticate_user!
 
--- app/controllers/api/v1/registered_players_controller.rb
+++ app/controllers/api/v1/registered_players_controller.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class Api::V1::RegisteredPlayersController < ActionController::API
   before_action :authenticate_user!
 
   def index
-    favorite_players = {batters: current_user.favorite_batters.all, pitchers: current_user.favorite_pitchers.all}
+    favorite_players = { batters: current_user.favorite_batters.all, pitchers: current_user.favorite_pitchers.all }
     render json: favorite_players
   end
 end
