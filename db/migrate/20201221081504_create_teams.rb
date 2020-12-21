d38f4b4976f47df25ac3d522211a5cca9ad08253
KishiKyosuke <carp011808pg@icloud.com>
2020-12-21 17:23:34 +0900

    `bin/rails g model Team`を実行

---
+++ db/migrate/20201221081504_create_teams.rb
@@ -0,0 +1,12 @@
+class CreateTeams < ActiveRecord::Migration[6.0]
+  def change
+    create_table :teams do |t|
+      t.string :name, null: false
+      t.string :formal_name, null: false
+      t.integer :ranking, null: false
+      t.integer :league, null: false
+
+      t.timestamps
+    end
+  end
+end
