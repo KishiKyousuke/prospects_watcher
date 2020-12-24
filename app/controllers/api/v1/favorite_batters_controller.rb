b0e39d75224d2c2d43624d9b1e4861805ee76cc2
KishiKyosuke <carp011808pg@icloud.com>
2020-12-24 11:06:33 +0900

    モデルにuniquenessバリデーションを追加
    - 各コントローラのcreateメソッドdestroyメソッドが例外を起こすように変更

--- app/controllers/api/v1/favorite_batters_controller.rb
+++ app/controllers/api/v1/favorite_batters_controller.rb
@@ -4,10 +4,10 @@ class Api::V1::FavoriteBattersController < ActionController::API
   before_action :authenticate_user!
 
   def create
-    current_user.favorite_batters.create(batter_id: params[:player_id])
+    current_user.favorite_batters.create!(batter_id: params[:player_id])
   end
 
   def destroy
-    current_user.favorite_batters.find_by(batter_id: params[:player_id]).destroy
+    current_user.favorite_batters.find_by(batter_id: params[:player_id]).destroy!
   end
 end
--- app/controllers/api/v1/favorite_pitchers_controller.rb
+++ app/controllers/api/v1/favorite_pitchers_controller.rb
@@ -4,10 +4,10 @@ class Api::V1::FavoritePitchersController < ActionController::API
   before_action :authenticate_user!
 
   def create
-    current_user.favorite_pitchers.create(pitcher_id: params[:player_id])
+    current_user.favorite_pitchers.create!(pitcher_id: params[:player_id])
   end
 
   def destroy
-    current_user.favorite_pitchers.find_by(pitcher_id: params[:player_id]).destroy
+    current_user.favorite_pitchers.find_by(pitcher_id: params[:player_id]).destroy!
   end
 end
