04d04206307f731bda82747a90eb95bd0909e68b
KishiKyosuke <carp011808pg@icloud.com>
2020-12-24 09:30:54 +0900

    bin/rails db:migrate を実行
    - favorite_batters/pitchersテーブルにunique indexを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_012845) do
+ActiveRecord::Schema.define(version: 2020_12_24_001646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_12_22_012845) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["batter_id"], name: "index_favorite_batters_on_batter_id"
+    t.index ["user_id", "batter_id"], name: "index_favorite_batters_on_user_id_and_batter_id", unique: true
     t.index ["user_id"], name: "index_favorite_batters_on_user_id"
   end
 
@@ -51,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_12_22_012845) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["pitcher_id"], name: "index_favorite_pitchers_on_pitcher_id"
+    t.index ["user_id", "pitcher_id"], name: "index_favorite_pitchers_on_user_id_and_pitcher_id", unique: true
     t.index ["user_id"], name: "index_favorite_pitchers_on_user_id"
   end
 
