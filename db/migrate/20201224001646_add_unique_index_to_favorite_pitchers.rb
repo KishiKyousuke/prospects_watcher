fef9dbdc2329574d92824fefa5d70ac042e591db
KishiKyosuke <carp011808pg@icloud.com>
2020-12-24 10:02:46 +0900

    rubocopを通す

--- db/migrate/20201224001646_add_unique_index_to_favorite_pitchers.rb
+++ db/migrate/20201224001646_add_unique_index_to_favorite_pitchers.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddUniqueIndexToFavoritePitchers < ActiveRecord::Migration[6.0]
   def change
     add_index :favorite_pitchers, [:user_id, :pitcher_id], unique: true
