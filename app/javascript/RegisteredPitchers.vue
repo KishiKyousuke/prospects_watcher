d888561766a26ca1ce55b968f8a1719ad64f0d75
KishiKyosuke <carp011808pg@icloud.com>
2021-01-04 16:15:56 +0900

    RegisteredPitchers.vueを作成

---
+++ app/javascript/RegisteredPitchers.vue
@@ -0,0 +1,160 @@
+<template>
+  <div id="app">
+    <vue-good-table
+      @on-selected-rows-change="selectionChanged"
+      :columns="columns"
+      :rows="pitchers"
+      :select-options="{
+        enabled: true,
+        selectionText: '選手を選択中',
+        clearSelectionText: 'キャンセル',
+      }"
+    >
+      <div slot="selected-row-actions">
+        <el-button type="danger" size="small" round @click="releaseProcessing">解除する</el-button>
+      </div>
+    </vue-good-table>
+  </div>
+</template>
+
+<script>
+import { csrfToken } from '@rails/ujs'
+import axios from 'axios'
+
+axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken()
+
+export default {
+  props: {
+    pitchers: {
+      type: Array,
+      require: true
+    }
+  },
+  data: function () {
+    return {
+      checkedPlayers: [],
+      columns: [
+        {
+          label: '背番号',
+          field: 'number',
+          type: 'number'
+        },
+        {
+          label: '選手名',
+          field: 'name',
+          sortable: false
+        },
+        {
+          label: 'チーム',
+          field: 'team',
+          sortable: false
+        },
+        {
+          label: '防御率',
+          field: 'earned_run_average',
+          type: 'number'
+        },
+        {
+          label: '勝',
+          field: 'win',
+          type: 'number',
+          firstSortType: 'desc'
+        },
+        {
+          label: '負',
+          field: 'lose',
+          type: 'number'
+        },
+        {
+          label: '投球回',
+          field: 'innings_pitched',
+          type: 'number',
+          firstSortType: 'desc'
+        },
+        {
+          label: '登板',
+          field: 'pitched',
+          type: 'number',
+          firstSortType: 'desc'
+        },
+        {
+          label: 'セーブ',
+          field: 'number_of_save',
+          type: 'number',
+          firstSortType: 'desc'
+        },
+        {
+          label: 'HP',
+          field: 'hold_point',
+          type: 'number',
+          firstSortType: 'desc'
+        },
+        {
+          label: '三振',
+          field: 'strikeout',
+          type: 'number',
+          firstSortType: 'desc'
+        },
+        {
+          label: '奪三振率',
+          field: 'strikeouts_per_nine_innings',
+          type: 'number',
+          firstSortType: 'desc'
+        },
+        {
+          label: 'K/BB',
+          field: 'strikeout_to_walk_ratio',
+          type: 'number',
+          firstSortType: 'desc'
+        },
+        {
+          label: 'WHIP',
+          field: 'walks_and_hits_per_innings_pitched',
+          type: 'number'
+        }
+      ]
+    }
+  },
+  methods: {
+    selectionChanged(params) {
+      this.checkedPlayers = params.selectedRows
+    },
+    releaseProcessing() {
+      this.releasePitchers()
+      this.releaseNotice()
+    },
+    releasePitchers() {
+      for (const player of this.checkedPlayers) {
+        axios.delete(`/api/v1/favorite_pitchers`, {data: {player_id: player['pitcher_id']}})
+        const index = this.pitchers.findIndex(pitcher => pitcher['pitcher_id'] === player['pitcher_id'])
+        this.pitchers.splice(index, 1)
+      }
+    },
+    releaseNotice() {
+      this.$notify({
+        title: '登録解除',
+        message: '選手を登録から解除しました',
+        type: 'warning'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+/deep/ .vgt-selection-info-row {
+  line-height: 32px;
+}
+
+/deep/ [type="checkbox"]:not(:checked) {
+  position: initial;
+  opacity: 0.5;
+  pointer-events: auto;
+}
+
+/deep/ [type="checkbox"]:checked {
+  position: initial;
+  opacity: 1.0;
+  pointer-events: auto;
+}
+</style>
