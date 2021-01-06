a045e31604eb46d5f31749b6bd1be8c0017968e0
KishiKyosuke <carp011808pg@icloud.com>
2021-01-06 09:43:55 +0900

    bin/rails g model ScrapingLog

---
+++ app/models/scraping_log.rb
@@ -0,0 +1,2 @@
+class ScrapingLog < ApplicationRecord
+end
---
+++ db/migrate/20210106004245_create_scraping_logs.rb
@@ -0,0 +1,7 @@
+class CreateScrapingLogs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :scraping_logs do |t|
+      t.timestamps
+    end
+  end
+end
