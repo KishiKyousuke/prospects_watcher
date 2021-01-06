6b768e0d8c7f07c8e07154576d68c366bfadeb7a
KishiKyosuke <carp011808pg@icloud.com>
2021-01-06 17:22:51 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_004245) do
+ActiveRecord::Schema.define(version: 2021_01_06_082058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2021_01_06_004245) do
   create_table "scraping_logs", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "result"
   end
 
   create_table "teams", force: :cascade do |t|
