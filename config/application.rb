7b45c1c8f4682e3977ab49797d576d2f704c8a27
KishiKyosuke <carp011808pg@icloud.com>
2021-03-09 23:46:34 +0900

    generateコマンドのRSpecに関する設定

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,11 @@ module ProspectsWatcher
     config.generators do |g|
       g.helper false
       g.assets false
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
     end
     config.i18n.default_locale = :ja
     config.time_zone = 'Asia/Tokyo'
