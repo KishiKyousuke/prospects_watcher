6f42ee27591b705ba44f2b37f08dbae976d64514
KishiKyosuke <carp011808pg@icloud.com>
2021-06-03 20:38:53 +0900

    Userモデルにメール認証の設定を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :favorite_pitchers, dependent: :destroy
   has_many :favorite_batters, dependent: :destroy
   has_many :pitchers, through: :favorite_pitchers
