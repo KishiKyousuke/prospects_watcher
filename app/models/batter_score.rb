125ad02bd130ea0f00d54549ccd1fab2009a8952
KishiKyousuke <carp011808pg@icloud.com>
2023-03-15 22:13:14 +0900

    batter_scoreを修正

--- app/models/batter_score.rb
+++ app/models/batter_score.rb
@@ -2,24 +2,24 @@
 
 class BatterScore
   def initialize(scores, team_id)
-    @number = scores[0]
-    @url = scores[1]
-    @name = scores[2].strip
+    @number = scores[1]
+    @url = scores[2]
+    @name = scores[3].strip
     @team_id = team_id
-    @batting_average = scores[3]
-    @plate_appearance = scores[5]
-    @at_bat = scores[6]
-    @hits = scores[7]
-    @home_run = scores[10]
-    @runs_batted_in = scores[12]
-    @stolen_base = scores[19]
-    @on_base_percentage = scores[22]
-    @on_base_plus_slugging = scores[24]
-    @walks = scores[15]
-    @hit_by_pitch = scores[16]
-    @scoring_position_batting_average = scores[25]
-    @strikeout = scores[14]
-    @error = scores[26]
+    @batting_average = scores[4]
+    @plate_appearance = scores[6]
+    @at_bat = scores[7]
+    @hits = scores[8]
+    @home_run = scores[11]
+    @runs_batted_in = scores[13]
+    @stolen_base = scores[20]
+    @on_base_percentage = scores[23]
+    @on_base_plus_slugging = scores[25]
+    @walks = scores[16]
+    @hit_by_pitch = scores[17]
+    @scoring_position_batting_average = scores[26]
+    @strikeout = scores[15]
+    @error = scores[27]
   end
 
   def reflect_in_db
