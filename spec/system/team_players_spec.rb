97a35f98abc601d4527ea9fb37bcad98505afd82
KishiKyousuke <carp011808pg@icloud.com>
2024-07-14 17:17:43 +0900

    選手一覧画面のテストを追加

--- spec/system/team_players_spec.rb
+++ spec/system/team_players_spec.rb
@@ -11,6 +11,10 @@ RSpec.describe '選手一覧画面', type: :system do
   describe 'チーム一覧の表示' do
     before { visit players_path }
 
+    it 'チーム一覧画面が表示されること' do
+      expect(page).to have_content 'チームから探す'
+    end
+
     it 'セリーグのチーム一覧が表示されること' do
       expect(page).to have_content '阪神タイガース'
       expect(page).to have_content '広島東洋カープ'
@@ -32,6 +36,62 @@ RSpec.describe '選手一覧画面', type: :system do
         expect(page).to have_content '東北楽天ゴールデンイーグルス'
         expect(page).to have_content '埼玉西武ライオンズ'
         expect(page).to have_content '北海道日本ハムファイターズ'
+      end
+    end
+  end
+
+  describe '選手一覧の表示' do
+    before { visit players_path }
+    let!(:team) { Team.find_by(name: '広島') }
+    let!(:batter) do
+      create(
+        :batter,
+        name: '田村 俊介',
+        team: team,
+        number: '60'
+      )
+    end
+    let!(:pitcher) do
+      create(
+        :pitcher,
+        name: '斉藤 優汰',
+        team: team,
+        number: '47'
+      )
+    end
+
+    before { visit players_path }
+
+    it 'チームをクリックするとそのチームの選手一覧が表示されること' do
+      click_on '広島東洋カープ'
+      expect(page).to have_content '野手'
+      expect(page).to have_content '背番号'
+      expect(page).to have_content '名前'
+      expect(page).to have_content '60'
+      expect(page).to have_content '田村 俊介'
+      expect(page).to have_content '投手'
+      expect(page).to have_content '47'
+      expect(page).to have_content '斉藤 優汰'
+    end
+
+    it '+ボタンを押すと選手が登録されること' do
+      click_on '広島東洋カープ'
+      first('button.light-green').click
+      expect(page).to have_content '登録完了'
+      expect(page).to have_content '選手を登録しました'
+    end
+
+    context '登録済みの選手の場合' do
+      before do
+        FavoriteBatter.create!(user: user, batter: batter)
+        visit players_path
+      end
+
+      it '-ボタンを押すと選手が登録解除されること' do
+        click_on '広島東洋カープ'
+        first('button.red').click
+        expect(page).to have_content '登録解除'
+        expect(page).to have_content '選手を登録から解除しました'
       end
     end
   end
